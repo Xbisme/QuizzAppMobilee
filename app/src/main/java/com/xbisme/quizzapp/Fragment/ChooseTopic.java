c5876e040f3cb7a8dd0faded705fe38210d5fff2
Minh Tam <mjnhtam3092002@gmail.com>
2022-11-29 15:42:30 +0700

    update layout_fragment and question

---
+++ app/src/main/java/com/xbisme/quizzapp/Fragment/ChooseTopic.java
@@ -0,0 +1,66 @@
+package com.xbisme.quizzapp.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xbisme.quizzapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChooseTopic#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChooseTopic extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ChooseTopic() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChooseTopic.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChooseTopic newInstance(String param1, String param2) {
+        ChooseTopic fragment = new ChooseTopic();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_topic, container, false);
+    }
+}
---
+++ app/src/main/java/com/xbisme/quizzapp/Question/AndroidQues.java
@@ -0,0 +1,49 @@
+package com.xbisme.quizzapp.Question;
+
+public class AndroidQues {
+    public class AndroidQues {
+        public static String[] easy_question = {
+                "Minh Tâm rất gà có đúng không?",
+                "Minh Đức là em Minh Tâm đúng không?",
+                "Hà Việt Dũng chỉ là 1 con gà có đúng không?",
+                "Cao Xuân Bình đẹp zai nhất phòng 403 đúng không?",
+                "Top 1 rank thách đấu Việt Nam là Cao Xuân Bình có đúng không"
+        };
+        public static String[] easy_correctAnswer= {
+                "Đúng",
+                "Sai",
+                "Đúng",
+                "Sai",
+                "Đúng"
+        };
+        public static String[] normal_question = {
+                "Minh Tâm rất gà có đúng không?",
+                "Minh Đức là em Minh Tâm đúng không?",
+                "Hà Việt Dũng chỉ là 1 con gà có đúng không?",
+                "Cao Xuân Bình đẹp zai nhất phòng 403 đúng không?",
+                "Top 1 rank thách đấu Việt Nam là Cao Xuân Bình có đúng không"
+        };
+        public static String[] normal_correctAnswer ={
+                "Đúng",
+                "Sai",
+                "Đúng",
+                "Sai",
+                "Đúng"
+        };
+        public static String[] hard_question = {
+                "Minh Tâm rất gà có đúng không?",
+                "Minh Đức là em Minh Tâm đúng không?",
+                "Hà Việt Dũng chỉ là 1 con gà có đúng không?",
+                "Cao Xuân Bình đẹp zai nhất phòng 403 đúng không?",
+                "Top 1 rank thách đấu Việt Nam là Cao Xuân Bình có đúng không"
+        };
+        public static String[] hard_correctAnswer = {
+                "Đúng",
+                "Sai",
+                "Đúng",
+                "Sai",
+                "Đúng"
+        };
+    }
+
+}
